61f8b4d9b28ed67e0a94b0add0bbb147367aae1b
Balasankar C <balasankarc@autistici.org>
2016-12-07 10:35:49 +0530

    Add service_enabled? method for OmnibusHelper

--- files/gitlab-cookbooks/gitlab/libraries/helper.rb
+++ files/gitlab-cookbooks/gitlab/libraries/helper.rb
@@ -97,12 +97,19 @@ end
 class OmnibusHelper
   extend ShellOutHelper
 
-  def self.should_notify?(service_name)
-    File.symlink?("/opt/gitlab/service/#{service_name}") && service_up?(service_name)
+  def self.should_notify?(node=nil, service_name)
+    File.symlink?("/opt/gitlab/service/#{service_name}") && service_up?(service_name) && service_enabled?(node, service_name)
   end
 
   def self.not_listening?(service_name)
     File.exists?("/opt/gitlab/service/#{service_name}/down") && service_down?(service_name)
+  end
+
+  def self.service_enabled?(node, service_name)
+    # Check if service is explicitly disabled in the node.
+    # This method doesn't apply if node argument is given and hence defaults
+    # to true for preventing abrupt exit.
+    node ? node['gitlab'][service_name]['enable'] : true
   end
 
   def self.service_up?(service_name)
